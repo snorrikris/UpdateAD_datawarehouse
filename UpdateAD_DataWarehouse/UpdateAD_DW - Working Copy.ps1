30d435385ebae691d4d0a986e46511253a682a78
Snorri Kristjánsson <snorri@veritas.is>
2016-01-01 17:48:27 +0000

    Powershell script final

---
+++ UpdateAD_DataWarehouse/UpdateAD_DW - Working Copy.ps1
@@ -0,0 +1,77 @@
+﻿############################################################################################################
+# Modify the following parameters:
+
+$TaskTitle = "Update Active Directory data warehouse on SQL server"
+
+# Email settings
+$MailTo = "snorri@veritas.is"
+$MailFrom = "dummy@dummy.com"
+$MailServer = "kerfispostur.veca.is"
+$SendMailOnSuccess = $true
+$SendMailOnFailure = $true
+
+$connString = "Data Source=snorridev\sqlexpress;Initial Catalog=AD_DW;Integrated Security=TRUE;"
+
+
+############################################################################################################
+# DO NOT MODIFY ANYTHING BELOW THIS LINE
+ 
+$SqlConnection = new-object System.Data.SqlClient.SqlConnection
+$SqlConnection.ConnectionString = $connString
+$result = 99
+$global:log = ""
+$global:sqllog = ""
+$now = Get-Date -Format G
+try
+{
+    #$Sqlconnection.FireInfoMessageEventOnUserErrors=$true
+    #Register-ObjectEvent -InputObject $SqlConnection -EventName InfoMessage -Action { Write-Host "$($Event.SourceEventArgs)" } -SupportEvent
+    Register-ObjectEvent -InputObject $SqlConnection -EventName InfoMessage -Action { $global:sqllog += "$($Event.SourceEventArgs)`n" } -SupportEvent
+    #Register-ObjectEvent -InputObject $SqlConnection -EventName InfoMessage -Action { $global:lop = $global:lop -join " XXX $($Event.SourceEventArgs)" } -SupportEvent
+
+    $global:log += "$now`tConnecting to SQL server...`n"
+    $SqlConnection.Open()
+
+    $SqlCommand = $SqlConnection.CreateCommand()
+    #$SqlCommand.CommandText = "EXECUTE [dbo].[usp_UpdateADcontacts] @ADpath;"
+    $SqlCommand.CommandText = "EXECUTE [dbo].[usp_UpdateADall_tables] @ADpath;"
+ 
+    # Add parameters to pass values to the stored procedure
+    $SqlCommand.Parameters.AddWithValue("@ADpath", "LDAP://DC=veca,DC=is") | Out-Null
+
+    $now = Get-Date -Format G
+    $global:log += "$now`tExecuting stored procedure...`n"
+    $result = $SqlCommand.ExecuteNonQuery()
+    Wait-Event -SourceIdentifier "Dummy" -Timeout 2    # Need to wait for all async events to complete.
+    $now = Get-Date -Format G
+    $global:log += "$now`tResult = $result`n"
+    $global:log += $global:sqllog
+}
+catch
+{
+    $now = Get-Date -Format G
+    $global:log += "$now`tTask failed`n"
+    $global:log += "`t$_`n"  #current error
+    $global:log += "`t(error) Result = $result`n"
+    $global:log += "`tOutput from SQL (if any)`n"
+    $global:log += $global:sqllog + "`n"
+}
+finally
+{
+    if ($SqlConnection.State -eq "Open") 
+    { 
+        $now = Get-Date -Format G
+        $global:log += "$now`tClosing Connection...`n" 
+        # Close the currently open connection. 
+        $SqlConnection.Close() 
+    } 
+}
+Write-Host $global:log
+if ($SendMailOnSuccess -eq $true -AND $result -eq 0)
+{
+    Send-MailMessage -To $MailTo -Subject "$TaskTitle - Task succeeded" -Body $global:log -SmtpServer $MailServer -From $MailFrom
+}
+if ($SendMailOnFailure -eq $true -AND $result -ne 0)
+{
+    Send-MailMessage -To $MailTo -Subject "$TaskTitle - Task failed" -Body $global:log -SmtpServer $MailServer -From $MailFrom
+}
--- UpdateAD_DataWarehouse/UpdateAD_DW.ps1
+++ UpdateAD_DataWarehouse/UpdateAD_DW.ps1
@@ -1,4 +1,14 @@
-﻿
+﻿# Note to run this script using Task Scheduler:
+# Create new task.
+# Set task action: Start a program
+#     Program/script: powershell
+#     Arguments:      -nologo -File "C:\Scripts\UpdateAD_DW.ps1"
+
+############################################################################################################
+# Modify the following parameters:
+
+$ADpath = "LDAP://DC=veca,DC=is"    # <-- your AD domain
+
 $TaskTitle = "Update Active Directory data warehouse on SQL server"
 
 # Email settings
@@ -8,63 +18,130 @@ $MailServer = "kerfispostur.veca.is"
 $SendMailOnSuccess = $true
 $SendMailOnFailure = $true
 
+#SQL connection string
 $connString = "Data Source=snorridev\sqlexpress;Initial Catalog=AD_DW;Integrated Security=TRUE;"
+
+
+############################################################################################################
+# DO NOT MODIFY ANYTHING BELOW THIS LINE
+
+$TaskStatus = $true
  
 $SqlConnection = new-object System.Data.SqlClient.SqlConnection
 $SqlConnection.ConnectionString = $connString
-$result = 99
 $global:log = ""
-$global:sqllog = ""
+
+$global:log += "************************************************************************`n"
+$global:log += "Task - $TaskTitle - starting`n"
+[string] $PSverMajor = $PSVersionTable.PSVersion.Major
+[string] $PSverMinor = $PSVersionTable.PSVersion.Minor
+$global:log += "`nPowerShell version: $PSverMajor.$PSverMinor`n"
+$global:log += "Computername: $env:computername`n"
+$global:log += "Username: $env:username`n"
 $now = Get-Date -Format G
+$global:log += "$now`tConnecting to SQL server...`n"
 try
 {
-    $Sqlconnection.FireInfoMessageEventOnUserErrors=$true
-    Register-ObjectEvent -InputObject $SqlConnection -EventName InfoMessage -Action { Write-Host "$($Event.SourceEventArgs)" } -SupportEvent
-    #Register-ObjectEvent -InputObject $SqlConnection -EventName InfoMessage -Action { $global:sqllog += "$($Event.SourceEventArgs)`n" } -SupportEvent
-    #Register-ObjectEvent -InputObject $SqlConnection -EventName InfoMessage -Action { $global:op = $global:op -join " XXX $($Event.SourceEventArgs)" } -SupportEvent
-
-    $global:log += "$now`tConnecting to SQL server...`n"
     $SqlConnection.Open()
-
-    $SqlCommand = $SqlConnection.CreateCommand()
-    $SqlCommand.CommandText = "EXECUTE [dbo].[usp_UpdateADcontacts] @ADpath;"
- 
-    # Add parameters to pass values to the stored procedure
-    $SqlCommand.Parameters.AddWithValue("@ADpath", "LDAP://DC=veca,DC=is") | Out-Null
-
-    $now = Get-Date -Format G
-    $global:log += "$now`tExecuting stored procedure...`n"
-    $result = $SqlCommand.ExecuteNonQuery()
-    Wait-Event -SourceIdentifier "Dummy" -Timeout 2    # Need to wait for all async events to complete.
-    $now = Get-Date -Format G
-    $global:log += "$now`tResult = $result`n"
-    $global:log += $global:sqllog
 }
 catch
 {
+    $TaskStatus = $false
     $now = Get-Date -Format G
-    $global:log += "$now`tTask failed"
-    $global:log += "`t$_"  #current error
-    $global:log += "`t(error) Result = $result`n"
-    $global:log += "`tOutput from SQL (if any)`n"
-    $global:log += $global:sqllog
+    $global:log += "$now`tOpen SQL connection failed`n"
+    $global:log += "Connection string: $connString`n"
+    $global:log += "$_`n"  #current error
 }
-finally
-{
-    if ($SqlConnection.State -eq "Open") 
-    { 
+
+function ExecuteUpdateAD_SP { 
+
+	[cmdletbinding()]
+
+	Param(
+		[Parameter(Position=0,Mandatory = $true)]
+		[ValidateNotNullorEmpty()]
+		[string]$SPcmdText,
+
+		[Parameter(Position=1,Mandatory = $true)]
+		[ValidateNotNullorEmpty()]
+		[string]$AD_Path
+
+		)
+	function Get-SQLConnectionEvent($EventID) {
+	
+		write-output (Get-Event -SourceIdentifier $EventID -ErrorAction SilentlyContinue  | 
+		Select -ExpandProperty SourceEventArgs | 
+		Select -ExpandProperty message) 
+		Remove-Event -SourceIdentifier $EventID -ErrorAction SilentlyContinue
+	}
+    try
+    {
+        $global:log += "`n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=`n"
         $now = Get-Date -Format G
-        $global:log += "$now`tClosing Connection...`n" 
-        # Close the currently open connection. 
-        $SqlConnection.Close() 
-    } 
+        $global:log += "$now`tExecuting stored procedure...`n$SPcmdText`nADpath: $AD_Path`n"
+
+        if ($SqlConnection.State -ne "Open") 
+        {
+            $global:log += "No connection to SQL server.`n"
+            return $false
+        }
+
+        [string] $evtguid = [guid]::NewGuid()
+	 	$eventID = "SQLConnectionEvent_$evtguid";
+	 	Register-ObjectEvent -inputObject $SqlConnection -eventName InfoMessage -sourceIdentifier $eventID
+
+        $SqlCommand = $SqlConnection.CreateCommand()
+        $SqlCommand.CommandText = $SPcmdText
+        $SqlCommand.Parameters.AddWithValue("@ADpath", $AD_Path) | Out-Null
+
+        $SqlCommand.ExecuteNonQuery() | Out-Null
+
+        # Get messages from SQL query
+        Wait-Event -SourceIdentifier $eventID -Timeout 2 | Out-Null   # Need to wait for all async events to complete.
+		$Message = Get-SQLConnectionEvent $eventID
+        $global:log += $Message
+    }
+    catch
+    {
+        $now = Get-Date -Format G
+        $global:log += "$now`tTask failed`n"
+        $global:log += "`t$_`n"  #current error
+        return $false
+    }
+    return $true
 }
-Write-Host $global:log
-if ($SendMailOnSuccess -eq $true -AND $result -eq 0)
+
+$cmds = @(
+    "EXECUTE [dbo].[usp_UpdateADusers] @ADpath;",
+	"EXECUTE [dbo].[usp_UpdateADcontacts] @ADpath;",
+	"EXECUTE [dbo].[usp_UpdateADcomputers] @ADpath;",
+	"EXECUTE [dbo].[usp_UpdateADwell_known_sids] @ADpath;",
+	"EXECUTE [dbo].[usp_UpdateADgroups] @ADpath;",
+	"EXECUTE [dbo].[usp_UpdateADusersPhotos] @ADpath;")
+
+foreach ($cmd in $cmds) {
+    $ExecResult = ExecuteUpdateAD_SP -SPcmdText $cmd -AD_Path $ADpath
+    if($ExecResult -eq $false) {
+        $TaskStatus = $false
+    }
+}
+
+if ($SqlConnection.State -eq "Open") 
+{ 
+    $now = Get-Date -Format G
+    $global:log += "`n`n$now`tClosing Connection...`n" 
+    $SqlConnection.Close() # Close the currently open connection.
+} 
+
+$global:log += "************************************************************************`n"
+$global:log += "Task - $TaskTitle - ends`n"
+#Write-Host $global:log
+
+if ($SendMailOnSuccess -eq $true -AND $TaskStatus -eq $true)
 {
     Send-MailMessage -To $MailTo -Subject "$TaskTitle - Task succeeded" -Body $global:log -SmtpServer $MailServer -From $MailFrom
 }
-if ($SendMailOnFailure -eq $true -AND $result -ne 0)
+if ($SendMailOnFailure -eq $true -AND $TaskStatus -eq $false)
 {
     Send-MailMessage -To $MailTo -Subject "$TaskTitle - Task failed" -Body $global:log -SmtpServer $MailServer -From $MailFrom
 }
